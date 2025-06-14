a70935ff9fb5bfd0aeb17efd34063177e05642ab
akarizidu2@gmail.com <akarizidu2@gmail.com>
2025-06-15 00:59:23 +0300

    feat: add convert images to png and push to wallpaper folder in config hypr

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,99 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/chai2010/webp"
 )
 
+var mimetypes = []string{"jpeg", "jpg", "png", "webp"}
+
+var decoders = map[string]func(i io.Reader) (image.Image, error){
+	"jpeg": jpeg.Decode,
+	"jpg":  jpeg.Decode,
+	"png":  png.Decode,
+	"webp": webp.Decode,
+}
+
+const outputPath = "/home/vyantik/.config/hypr/wallpaper/wallpaper.png"
+
 func main() {
-	imgPath := flag.String("path", "", "path to the image you want to change the wallpaper insted of current one")
+	imgPathPtr := flag.String("path", "", "path to the image you want to change the wallpaper insted of current one")
 	flag.Parse()
 
-	if *imgPath == "" {
-		fmt.Println("pass the image path like 'chpaper --path path/to/image.png'")
+	if *imgPathPtr == "" {
+		log.Fatalln("pass the image path like 'chpaper --path path/to/image.png'")
 		return
 	}
 
-	fmt.Println(*imgPath)
+	imgPath := *imgPathPtr
+	if !fileExist(imgPath) {
+		log.Fatalln("pass the image, not dir")
+	}
+	mimetypeFromSplit := strings.Split(imgPath, ".")[1:]
+	if len(mimetypeFromSplit) < 1 {
+		log.Fatalln("is not image")
+	}
+	if isValidImage := isImage(mimetypeFromSplit[0]); isValidImage == false {
+		log.Fatalln("is not image")
+	}
+
+	convertImageToPNG(imgPath, outputPath)
+}
+
+func fileExist(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		log.Fatalln("image doesn't exist through this path")
+	}
+	if err != nil {
+		log.Fatalln("internal error")
+	}
+	return !info.IsDir()
+}
+
+func isImage(filetype string) bool {
+	return slices.Contains(mimetypes, filetype)
+}
+
+func convertImageToPNG(inputPath, outputPath string) {
+
+	log.Println("outputPath:", outputPath)
+
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatalln("failed to open input file")
+	}
+	defer inFile.Close()
+
+	ext := strings.ToLower(strings.TrimPrefix(filepath.Ext(inputPath), "."))
+
+	decodeFunc, ok := decoders[ext]
+	if !ok {
+		log.Fatalf("unsupported input image format.")
+	}
+
+	img, err := decodeFunc(inFile)
+	if err != nil {
+		log.Fatalln("failed to decode image from")
+	}
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatalln("failed to create output file")
+	}
+	defer outFile.Close()
+
+	err = png.Encode(outFile, img)
+	if err != nil {
+		log.Fatalln("failed to encode image to PNG")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vyantik/chpaper
 
 go 1.24.4
+
+require github.com/chai2010/webp v1.4.0 // indirect
