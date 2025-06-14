e8446ca9c5e753924df26f47fdb950f136adf833
akarizidu2@gmail.com <akarizidu2@gmail.com>
2025-06-14 23:19:51 +0300

    feat: add flag path logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hi my chpaper")
+	imgPath := flag.String("path", "", "path to the image you want to change the wallpaper insted of current one")
+	flag.Parse()
+
+	if *imgPath == "" {
+		fmt.Println("pass the image path like 'chpaper --path path/to/image.png'")
+		return
+	}
+
+	fmt.Println(*imgPath)
 }
